497538f914fbd41fa30d35af8a3f6ea95c0e481f
andrej11614 <47032530+andrej11614@users.noreply.github.com>
2019-03-28 07:57:20 +0100

    Add files via upload

---
+++ Lab1_173207/Lab1/Lab1/Lab12.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    class Lab12
+    {
+       /* static void Main(string[] args)
+        {
+            int k;
+            int[] arr=new int[0];
+            while (true)
+            {
+                Console.WriteLine("Slucajno Generirani - 0");
+                Console.WriteLine("Manuelno Vnesuvanje - 1");
+
+                try
+                {
+                    k = Convert.ToInt32(Console.ReadLine());
+                    if (k != 1 && k != 0) continue;
+                    else break;
+
+                }
+                catch (Exception e) {
+                    continue;
+                }
+            }
+            if (k == 1)
+            {
+                int N = Convert.ToInt32(Console.ReadLine());
+
+                arr = new int[N];
+
+                for (int i = 0; i < N; i++)
+                {
+                    arr[i] = Convert.ToInt32(Console.ReadLine());
+                }
+
+
+            }
+            if (k == 0) {
+                Random rnd = new Random();
+                int N = rnd.Next(1,10);
+                arr = new int[N];
+                for (int i = 0; i < N; i++)
+                {
+                    arr[i] = rnd.Next(1,1000);
+                }
+            }
+            Console.WriteLine(arr.Sum() / arr.Length+" prosecno");
+            Console.WriteLine(arr.Max()+" max");
+            Console.WriteLine(arr.Min()+" min");
+            Console.ReadKey();
+        }*/
+    }
+}
---
+++ Lab1_173207/Lab1/Lab1/Lab14.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    enum PasswordStrenght
+    {
+        easy,
+        normal,
+        hard
+    }
+    class Lab14
+    {
+        static void Main(string[] args)
+        {
+            /*string myString = "This is a test.";
+            char[] separator = { ' ' };
+            string[] myWords;
+            myWords = myString.Split(separator);
+            foreach (string word in myWords)
+            {
+                Console.WriteLine("{0}", word);
+            }
+            Console.ReadKey();*/
+            Console.WriteLine("Enter 3 passwords in 1 line:");
+            string passLine = Console.ReadLine();
+            char[] delimiter = { ' ' };
+            string[] parts = passLine.Split(delimiter);
+
+            string[] passwords = new string[3];
+
+            passwords[0] = generatePassword(PasswordStrenght.easy);
+            passwords[1] = generatePassword(PasswordStrenght.normal);
+            passwords[2] = generatePassword(PasswordStrenght.hard);
+
+            Console.WriteLine("FDSfADDASFASF");
+            for (int i = 0; i < parts.Length; i++)
+            {
+                if (passwords[i].Equals(parts[i])) {
+                    Console.WriteLine("You guessed parts[i]");
+                }
+            }
+            foreach(String p in passwords){
+                Console.WriteLine(p);
+            }
+            Console.ReadKey();
+        }
+        static string generatePassword
+            (PasswordStrenght passwordStrenght)
+        {
+            Random rnd = new Random();
+            if (passwordStrenght == PasswordStrenght.easy) {
+                const string valid = "abcdefghijklmnopqrstuvwxyz";
+                int length = rnd.Next(1,7);
+                StringBuilder sb = new StringBuilder();
+                while (0 < length--) {
+                    sb.Append(valid[rnd.Next(valid.Length)]);       
+                }
+                return sb.ToString();
+            }
+            if (passwordStrenght == PasswordStrenght.normal) {
+                const string valid = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+                int length = rnd.Next(6, 11);
+                StringBuilder sb = new StringBuilder();
+                while (0 < length--)
+                {
+                    sb.Append(valid[rnd.Next(valid.Length)]);
+                }
+                return sb.ToString();
+            }
+            if (passwordStrenght == PasswordStrenght.hard)
+            {
+                const string valid = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*";
+                int length = rnd.Next(10, 20);
+                StringBuilder sb = new StringBuilder();
+                while (0 < length--)
+                {
+                    sb.Append(valid[rnd.Next(valid.Length)]);
+                }
+                return sb.ToString();
+            }
+            throw new ArgumentOutOfRangeException();
+        }
+    }
+}
