7de3091fb90cbbc1a1909c9afc290b4944e52500
andrej11614 <47032530+andrej11614@users.noreply.github.com>
2019-05-01 23:33:26 +0200

    Add files via upload

---
+++ Pacman/Pacman/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pacman
+{
+    public partial class Form1 : Form
+    {
+        Timer timer;
+        //public static Graphics g;
+        Pacman pacman;
+        static readonly int TIMER_INTERVAL = 250;
+        static readonly int WORLD_WIDTH = 15;
+        static readonly int WORLD_HEIGHT = 10;
+        Image foodImage;
+        bool[][] foodWorld;
+        public Form1()
+        {
+            InitializeComponent();
+            foodImage = Properties.Resources.pear;
+            //foodImage.Width = Pacman.RADIUS * 2;
+            
+            DoubleBuffered = true;
+            newGame();
+        }
+
+        void newGame() {
+            pacman = new Pacman();
+            //Thread.Sleep(3000);
+            this.Width = Pacman.RADIUS * 2 * (WORLD_WIDTH + 1);
+            this.Height = Pacman.RADIUS * 2 * (WORLD_HEIGHT + 1);
+
+            foodWorld = new bool[WORLD_WIDTH][];
+            for (int i = 0; i < WORLD_WIDTH; i++)
+            {
+                foodWorld[i] = new bool[WORLD_HEIGHT];
+                for (int j = 0; j < WORLD_HEIGHT; j++)
+                {
+                    foodWorld[i][j] = true;
+                }
+            }
+            
+            timer = new Timer();
+            timer.Interval = TIMER_INTERVAL;
+            timer.Tick += new EventHandler(timer_Tick);
+            timer.Start();
+            //pacman.Move(0,0);
+            Invalidate();
+        }
+
+        void timer_Tick(object sender, EventArgs e) {
+            for (int i = 0; i < WORLD_WIDTH; i++)
+            {
+                for (int j = 0; j < WORLD_HEIGHT; j++)
+                {
+                    foodWorld[pacman.posX][pacman.posY] = false;
+                }
+            }
+            pacman.Move(WORLD_WIDTH,WORLD_HEIGHT);
+            Invalidate();
+        }
+
+        /*private void Canvas_Paint(object sender, PaintEventArgs e)
+        {
+            // Graphics g = canvas.CreateGraphics();
+            //pacman.Draw(g);
+            Graphics g = e.Graphics;
+            g.Clear(Color.White);
+            for (int i = 0; i < foodWorld.Length; i++)
+            {
+                for (int j = 0; j < foodWorld[i].Length; j++)
+                {
+                    if (foodWorld[i][j]) {
+                        g.DrawImageUnscaled(foodImage,i,j);
+                        //g.DrawImage(foodImage,i,j);
+                    }
+                }
+            }
+            pacman.Draw(g);
+        }*/
+        
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            g.Clear(Color.White);
+            for (int i = 0; i < foodWorld.Length; i++)
+            {
+                for (int j = 0; j < foodWorld[i].Length; j++)
+                {
+                    if (foodWorld[i][j])
+                    {
+                        g.DrawImageUnscaled(foodImage, i,j);
+                        //g.DrawImage(foodImage,i,j);
+                    }
+                }
+            }
+            pacman.Draw(g);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Pacman/Pacman/Pacman.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+
+namespace Pacman
+{
+    class Pacman
+    {
+        public enum DIRECTION{
+            LEFT,UP,RIGHT,DOWN
+        }
+        
+        public static Brush brush=
+            new SolidBrush(Color.Yellow);
+
+        public int posX { get; set; }
+        public int posY { get; set; }
+        public const int RADIUS= 20;
+        public float moveSpeed { get; set; }
+        public bool isMouthOpen { get; set; }
+        public DIRECTION direction;
+
+        public Pacman() {
+
+            moveSpeed = RADIUS;
+            posX = 7;
+            posY = 5;
+            direction = DIRECTION.RIGHT;
+            isMouthOpen = false;
+        }
+
+        public void Draw(Graphics g) {
+            if (isMouthOpen)
+            {
+                g.FillPie(brush, posX, posY, RADIUS * 2, RADIUS * 2, 45, 270);
+            }
+            else {
+                g.FillPie(brush, posX, posY, RADIUS*2, RADIUS*2, 5,350);
+            }
+        }
+        public void ChangeDirection(DIRECTION direction)
+        {
+            this.direction = direction;
+            isMouthOpen = !isMouthOpen;
+        }
+        public void Move(int x, int y) {
+            //posX += 30;
+            //Graphics g = Form1.g;
+            //Draw(g);
+
+        }
+    }
+}
